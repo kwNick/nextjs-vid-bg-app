f607d2947db974428f82f422e1dfcfa9bbeb83ed
Nickolas Piraino <kwnp17c@gmail.com>
2024-04-03 19:16:38 -0400

    fixed background using my own styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center p-24 min-h-screen">
-
-      <video src={require('../../public/above-the-clouds.mp4')} autoPlay muted className="absolute z-0 min-h-full min-w-full object-cover" />
-      <div className="z-1">
+    <main className="min-w-full min-h-screen">
+      <video src={require('../../public/above-the-clouds.mp4')} autoPlay loop muted className="min-h-full min-w-full object-cover" />
+      <div className="absolute min-h-full min-w-full inset-y-0 flex flex-col items-center justify-center text-white uppercase font-xl text-xl tracking-wider">
         <h1>UpInTheAir Studios</h1>
-        <div className="flex items-center justify-center uppercase text-white">
-          <button>our Work</button>
+        <div className="flex items-center justify-center">
+          <button className="p-8  ">our Work</button>
           <button>Our Story</button>
         </div>
       </div>
