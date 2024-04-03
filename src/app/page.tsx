cbeec2becbe28e8461acda1672c3c45ffb787517
Nickolas Piraino <kwnp17c@gmail.com>
2024-04-03 19:03:53 -0400

    trying to get video bg to go to z-index: -1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-center p-24 min-h-screen">
 
+      <video src={require('../../public/above-the-clouds.mp4')} autoPlay muted className="absolute z-0 min-h-full min-w-full object-cover" />
+      <div className="z-1">
+        <h1>UpInTheAir Studios</h1>
+        <div className="flex items-center justify-center uppercase text-white">
+          <button>our Work</button>
+          <button>Our Story</button>
+        </div>
+      </div>
     </main>
   );
 }
