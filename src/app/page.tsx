f0a8f659c0324cdc170197d0a770831e59a1e280
Nickolas Piraino <kwnp17c@gmail.com>
2024-04-04 14:43:25 -0400

    review and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
       <video src={require('../../public/above-the-clouds.mp4')} autoPlay loop muted className="min-h-full min-w-full object-cover" />
       <div className="absolute min-h-full min-w-full inset-y-0 flex flex-col items-center justify-center text-white uppercase font-xl text-xl tracking-wider">
         <h1>UpInTheAir Studios</h1>
-        <div className="flex items-center justify-center">
-          <button className="p-8  ">our Work</button>
+        <div className="flex items-center ">
+          <button className="p-6  ">Our Work</button>
           <button>Our Story</button>
         </div>
       </div>
